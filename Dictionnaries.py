aea951317a0a73244fd506fca356deadbef67e6d
DSVences <DucSoanVences99@gmail.com>
2020-03-25 20:59:10 +0700

    Learn dictionnaries type

---
+++ Dictionnaries.py
@@ -0,0 +1,22 @@
+### Add "Jake" to the phonebook with the phone number 938273443, and remove Jill from the phonebook.
+
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+
+# write your code here
+phonebook["Jake"] = 938273443
+del phonebook["Jill"] # phonebook.pop("Jill")
+
+# testing code
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
+
+# Iterating over dictionaries
+print("--------------------------------------------")
+for name, number in phonebook.items():
+    print("Phone number of %s is %d" % (name, number))
